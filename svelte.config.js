dc48534557557e3fa5e055cba5fc9acf952357ed
Devrock <devbhakat05@gmail.com>
2025-09-15 18:15:59 +0530

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -3,21 +3,14 @@ import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  // If you’re using SCSS/TypeScript, keep preprocess
   preprocess: preprocess(),
 
   kit: {
     adapter: adapter({
-      // by default it outputs to "build", which matches your netlify.toml
       edge: false,
       split: false
-    }),
-    alias: {
-      $components: 'src/components',
-      $lib: 'src/lib',
-      $routes: 'src/routes',
-      $styles: 'src/styles'
-    }
+    })
+    // 🚫 Removed "alias" because your SvelteKit version doesn't support it
   }
 };
 
